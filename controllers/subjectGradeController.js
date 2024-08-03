ea8ba16b82525c42a3da0f08df184a8621a46021
AkshandraSingh <nameste380@gmail.com>
2024-08-04 04:43:12 +0530

    Update: addedUpdateGrade

--- controllers/subjectGradeController.js
+++ controllers/subjectGradeController.js
@@ -20,6 +20,26 @@ const addGrade = async (req, res) => {
     }
 }
 
+//? Update Grade API
+const updateGrade = async (req, res) => {
+    try {
+        const { gradeId } = req.params
+        const updatedGrade = await subjectGradeModel.findByIdAndUpdate(gradeId, req.body, { new: true })
+        res.status(200).send({
+            success: true,
+            message: "Grade updated successfully",
+            updateGrade: updatedGrade,
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error!",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
     addGrade,
+    updateGrade,
 }
--- routes/subjectGradeRoute.js
+++ routes/subjectGradeRoute.js
@@ -6,5 +6,6 @@ const subjectGradeRouter = express.Router()
 
 //* Subject Grade Routes (Ending Points)
 subjectGradeRouter.post('/addGrade/:userId', subjectGradeController.addGrade)
+subjectGradeRouter.patch('/updateGrade/:gradeId', subjectGradeController.updateGrade)
 
 module.exports = subjectGradeRouter
