892f83de52e25840d2cb8ea553b0011841849c02
AkshandraSingh <nameste380@gmail.com>
2024-08-07 00:29:41 +0530

    Update: addedUserValidations

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,14 @@
 const express = require('express')
 
 const userController = require('../controllers/userController')
+const userValidator = require('../validations/userValidations/userValidator')
 
 const userRoute = express.Router()
 
-userRoute.post('/registerUser', userController.registerUser)
-userRoute.post('/loginUser', userController.loginUser)
+userRoute.post('/registerUser', userValidator.registerUserValidation, userController.registerUser)
+userRoute.post('/loginUser', userValidator.userLoginValidation, userController.loginUser)
 userRoute.post('/forgetPassword', userController.forgetPassword)
-userRoute.post('/resetPassword/:userId/:accessToken', userController.resetPassword)
-userRoute.post('/setNewPassword/:userId', userController.setNewPassword)
+userRoute.post('/resetPassword/:userId/:accessToken', userValidator.resetPasswordValidation, userController.resetPassword)
+userRoute.post('/setNewPassword/:userId', userValidator.setNewPasswordValidation, userController.setNewPassword)
 
 module.exports = userRoute
